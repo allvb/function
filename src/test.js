fed4882937dfe2d5e0635c046d71e5828d137842
All <allvb@rambler.ru>
2023-04-05 21:08:14 +0300

    Исправлен test

--- src/test.js
+++ src/test.js
@@ -2,14 +2,25 @@ import life from './life';
 
 const input = [
   { name: 'Маг', health: '60' },
-  // { name; 'Воин', health: '90' },
-  // { name; 'Лучница', health: '10' },
-  // { name; 'Маг', health: '20' }
+  { name: 'Воин', health: '110' },
+  { name: 'Лучница', health: '10' },
+  { name: 'Маг', health: '20' },
 ];
 
-test.('getLife', () => {
-  const expected = 'healthy';
-  const result = life(input[0]);
+test('getLife', () => {
+  let expected = 'healthy';
+  let result = life(input[0]);
+  expect(result).toBe(expected);
 
+  expected = false;
+  result = life(input[1]);
+  expect(result).toBe(expected);
+
+  expected = 'critical';
+  result = life(input[2]);
+  expect(result).toBe(expected);
+
+  expected = 'wounded';
+  result = life(input[3]);
   expect(result).toBe(expected);
 });
